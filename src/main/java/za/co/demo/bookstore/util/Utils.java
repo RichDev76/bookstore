b2e2997a9e5256633fda32fc0d8e061bcc56588b
RichDev76 <0ting-campo@icloud.com>
2023-09-12 16:24:30 +0200

    fixed config

--- src/main/java/za/co/demo/bookstore/util/Utils.java
+++ src/main/java/za/co/demo/bookstore/util/Utils.java
@@ -3,7 +3,6 @@ package za.co.demo.bookstore.util;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Utils {
-
     public static long random() {
         long smallest = 1000_0000;
         long biggest =  9999_9999;
